efe069bb814e355fac3d0436945ec6c31c82edf6
Melly2206 <melly.dwisuherlina@gmail.com>
2023-11-10 20:55:56 +0700

    membuat halaman barang

---
+++ screen/halaman/barang/index.js
@@ -0,0 +1,43 @@
+// src/components/FlowerList.js
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+const FlowerList = ({ flowers }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Flower List</Text>
+      <FlatList
+        data={flowers}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.flowerItem}>
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  flowerItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingVertical: 10,
+  },
+});
+
+export default FlowerList;
